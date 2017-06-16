4103e4956578927267f049e14d284f46a99229b3
jinshubao <jinshubao@mail.xusenintl.com>
2017-06-16 18:35:28 +0800

    update

--- api-server-1/src/main/java/com/jean/cloud/server/ApiApplication.java
+++ api-server-1/src/main/java/com/jean/cloud/server/ApiApplication.java
@@ -3,6 +3,9 @@ package com.jean.cloud.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,13 +13,20 @@ import org.springframework.web.client.RestTemplate;
 public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        ApplicationContext context = SpringApplication.run(ApiApplication.class, args);
+
+        System.out.println("---------");
     }
 
     @Bean
     @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    Sampler defaultSampler() {
+        return new AlwaysSampler();
     }
 
 }
--- api-server-1/src/test/java/com/jean/cloud/server/api/ApiControllerTest.java
+++ api-server-1/src/test/java/com/jean/cloud/server/api/ApiControllerTest.java
@@ -1,11 +1,6 @@
 package com.jean.cloud.server.api;
 
-import io.github.swagger2markup.Swagger2MarkupConverter;
 import org.junit.Test;
-
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * Created by jinshubao on 2017/6/7.
@@ -15,13 +10,6 @@ public class ApiControllerTest {
 
     @Test
     public void userDetail() throws Exception {
-        URL remoteSwaggerFile = new URL("http://petstore.swagger.io/v2/swagger.json");
-        Path outputDirectory = Paths.get("doc/gen");
-
-        Swagger2MarkupConverter.from(remoteSwaggerFile)
-                .build()
-                .toFolder(outputDirectory);
-
     }
 
 }
