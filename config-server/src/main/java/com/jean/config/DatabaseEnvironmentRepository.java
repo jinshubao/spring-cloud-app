e2090997a1eefd3af242f5dcd4a980a9583526cc
jinshubao <jinshubao@mail.xusenintl.com>
2017-07-27 15:24:28 +0800

    update

---
+++ config-server/src/main/java/com/jean/config/DatabaseEnvironmentRepository.java
@@ -0,0 +1,9 @@
+package com.jean.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("spring.cloud.config.server.db")
+public class DatabaseEnvironmentRepository {
+
+
+}
--- config-server/src/main/java/com/jean/environment/DatabaseEnvironmentRepository.java
+++ config-server/src/main/java/com/jean/environment/DatabaseEnvironmentRepository.java
@@ -3,42 +3,32 @@ package com.jean.environment;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.config.environment.Environment;
 import org.springframework.cloud.config.environment.PropertySource;
 import org.springframework.cloud.config.server.environment.AbstractScmEnvironmentRepository;
+import org.springframework.cloud.config.server.environment.EnvironmentRepository;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 import java.util.HashMap;
 
 @ConfigurationProperties("spring.cloud.config.server.db")
-public class DatabaseEnvironmentRepository extends AbstractScmEnvironmentRepository {
 
-    private static Logger logger = LoggerFactory.getLogger(DatabaseEnvironmentRepository.class);
+@ConditionalOnBean(ConfigurableEnvironment.class)
+public class DatabaseEnvironmentRepository implements EnvironmentRepository {
 
-    public DatabaseEnvironmentRepository(ConfigurableEnvironment environment) {
-        super(environment);
-    }
+    @Autowired
+    DatabaseEnvironmentRepository environment;
 
     @Override
     public Environment findOne(String application, String profile, String label) {
-        logger.info("----------------------------------------------findOne");
         Environment environment = new Environment(application, new String[]{profile}, label, "", "");
         HashMap<String, Object> configs = new HashMap<>();
-        configs.put("com.jean.name","database config name-------------");
-        configs.put("com.jean.title","database config title");
-        environment.add(new PropertySource(getUri(),configs ));
+        configs.put("com.jean.name", "database config name-------------");
+        configs.put("com.jean.title", "database config title");
+        environment.add(new PropertySource(environment.getUrl(), configs));
         return environment;
-    }
-
-    @Override
-    public Locations getLocations(String application, String profile, String label) {
-        logger.info("----------------------------------------------getLocations");
-        return new Locations(application, profile, label, "", new String[]{});
-    }
-
-    @Override
-    public int getOrder() {
-        return 0;
     }
 }
