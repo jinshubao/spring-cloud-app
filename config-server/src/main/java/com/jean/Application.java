f7a94db26a33f40095be7ffa009d37883bdae445
jinshubao <jinshubao@mail.xusenintl.com>
2017-07-27 15:08:09 +0800

    update

--- config-server/src/main/java/com/jean/Application.java
+++ config-server/src/main/java/com/jean/Application.java
@@ -1,9 +1,12 @@
 package com.jean;
 
+import com.jean.environment.DatabaseEnvironmentRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -12,5 +15,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public DatabaseEnvironmentRepository databaseEnvironmentRepository(ConfigurableEnvironment environment) {
+        return new DatabaseEnvironmentRepository(environment);
     }
 }
--- config-server/src/main/java/com/jean/environment/DatabaseEnvironmentRepository.java
+++ config-server/src/main/java/com/jean/environment/DatabaseEnvironmentRepository.java
@@ -5,8 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.config.environment.PropertySource;
 import org.springframework.cloud.config.server.environment.AbstractScmEnvironmentRepository;
 import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.HashMap;
 
 @ConfigurationProperties("spring.cloud.config.server.db")
 public class DatabaseEnvironmentRepository extends AbstractScmEnvironmentRepository {
@@ -19,14 +22,19 @@ public class DatabaseEnvironmentRepository extends AbstractScmEnvironmentReposit
 
     @Override
     public Environment findOne(String application, String profile, String label) {
-        logger.info("findOne");
-        return null;
+        logger.info("----------------------------------------------findOne");
+        Environment environment = new Environment(application, new String[]{profile}, label, "", "");
+        HashMap<String, Object> configs = new HashMap<>();
+        configs.put("com.jean.name","database config name-------------");
+        configs.put("com.jean.title","database config title");
+        environment.add(new PropertySource(getUri(),configs ));
+        return environment;
     }
 
     @Override
     public Locations getLocations(String application, String profile, String label) {
-        logger.info("getLocations");
-        return null;
+        logger.info("----------------------------------------------getLocations");
+        return new Locations(application, profile, label, "", new String[]{});
     }
 
     @Override
